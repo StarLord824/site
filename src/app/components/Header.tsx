fa685d948ba6827f9240221b3367f442df5284b1
abhinavpc4 <shuklaabhinav824@gmail.com>
2025-04-23 19:44:20 +0530

    improved header ui

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,20 +1,36 @@
 import React from 'react'
+import SearchBar from './SearchBar'
 
 const Header = () => {
+  const tabs= [
+    {
+      title: 'Projects',
+      link: '/projects'
+    },{
+      title: 'Blogs',
+      link: '/blogs'
+    },{
+      title: 'Courseworks',
+      link: '/courseworks'
+    }]
   return (
-    <header className="flex items-center justify-around w-full h-1/9 border-b-blue-700 border-b-2 border-b-solid">
-        <div>StarLord.dev</div>
-        <div>
-          <ul className="flex space-x-4">
-            <li>Projects</li>
-            <li>Blogs</li>
-            <li>CourseWorks</li>
-          </ul>
+    <header className="flex items-center justify-around w-full h-1/9 border-b-2 border-white/20 text-xl font-semibold">
+        <div className='text-3xl font-bold'>StarLord.dev</div>
+        <div className='flex gap-3 text-xl font-semibold text-neutral-500'>
+            {tabs.map( (tab, index)=>{
+              return (
+                <div key={index} className='flex items-center justify-center hover:text-white hover:bg-neutral-800 rounded-lg h-12 w-40 p-2 transition ease-in'>
+                  <button><a href={tab.link}>{tab.title}</a></button>
+                </div>)
+            })
+          }
         </div>
-        <div className="flex space-x-4">
-          <div> Search </div>
+        <div className="flex gap-10">
+          <SearchBar/>
           <div>Dark/Light</div>
-          <div> Options</div>
+          <div> 
+            Options
+          </div>
         </div>
       </header>
   )
---
+++ src/app/components/SearchBar.tsx
@@ -0,0 +1,10 @@
+
+const SearchBar = () => {
+  return (
+    <div>
+        <input type="text" placeholder="Search..." className="w-64 h-10 text-center rounded-lg bg-neutral-800 text-white font-normal text-sm p-2 focus:outline-none focus:ring-2 focus:ring-blue-500" />
+    </div>
+  )
+}
+
+export default SearchBar
